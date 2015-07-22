6861c33abe73bd0e041b123ee58bc7e1297dd235
Dao Hoang Son <daohoangson@gmail.com>
2015-07-23 04:42:50 +0700

    added support for memcache in heroku.
    Environment variables: MEMCACHIER_SERVERS, MEMCACHIER_USERNAME, MEMCACHIER_PASSWORD

--- utils/other.go
+++ utils/other.go
@@ -6,7 +6,9 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/bmizerany/mc"
 	"github.com/daohoangson/go-socialcounters/services"
 )
 
@@ -33,11 +35,22 @@ func (u Other) HttpClient() *http.Client {
 }
 
 func (u Other) MemorySet(key string, value []byte, ttl int64) error {
-	return errors.New("Not implemented")
+	conn := getMcConn(u)
+	if conn == nil {
+		return nil
+	}
+
+	return conn.Set(key, string(value), 0, 0, int(ttl))
 }
 
 func (u Other) MemoryGet(key string) ([]byte, error) {
-	return nil, errors.New("Not implemented")
+	conn := getMcConn(u)
+	if conn == nil {
+		return nil, errors.New("No memcache connection")
+	}
+
+	value, _, _, err := conn.Get(key)
+	return []byte(value), err
 }
 
 func (u Other) DbSet(key string, hash map[string]string) error {
@@ -50,4 +63,38 @@ func (u Other) DbGet(key string) (map[string]string, error) {
 
 func (u Other) Logf(format string, args ...interface{}) {
 	log.Printf(format, args...)
+}
+
+var mcConn *mc.Conn
+var mcPrepared = false
+func getMcConn(u Other) *mc.Conn {
+	if !mcPrepared {
+		if addr := os.Getenv("MEMCACHIER_SERVERS"); addr != "" {
+			if m, err := mc.Dial("tcp", addr); err == nil {
+				username := os.Getenv("MEMCACHIER_USERNAME")
+				password := os.Getenv("MEMCACHIER_PASSWORD")
+
+				if username != "" && password != "" {
+					// only try to authenticate if both username and password are set
+					err = m.Auth(os.Getenv("MEMCACHIER_USERNAME"), os.Getenv("MEMCACHIER_PASSWORD"))
+					if err == nil {
+						u.Logf("Other.getMcConn: mc.Auth ok")
+						mcConn = m
+					} else {
+						u.Logf("Other.getMcConn: mc.Auth error %v", err)
+					}
+				} else {
+					// most of the case, the server does not require authentication
+					u.Logf("Other.getMcConn: mc.Dial ok")
+					mcConn = m
+				}
+			} else {
+				u.Logf("Other.getMcConn: mc.Dial error %v", err)
+			}
+		}
+
+		mcPrepared = true
+	}
+
+	return mcConn
 }
